45ad574844e03e118ff09084c25d2eca18395ba7
kris1027 <krzys89@gmail.com>
2026-01-19 11:01:01 +0100

    feat: add updatedAt field to Todo model and sync frontend types

---
+++ backend/prisma/migrations/20260119093811_add_updated_at/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - Added the required column `updateAt` to the `Todo` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Todo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Todo" ("completed", "createdAt", "description", "id", "title") SELECT "completed", "createdAt", "description", "id", "title" FROM "Todo";
+DROP TABLE "Todo";
+ALTER TABLE "new_Todo" RENAME TO "Todo";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
---
+++ backend/prisma/migrations/20260119095729_add_updated_at/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updateAt` on the `Todo` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `Todo` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Todo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Todo" ("completed", "createdAt", "description", "id", "title") SELECT "completed", "createdAt", "description", "id", "title" FROM "Todo";
+DROP TABLE "Todo";
+ALTER TABLE "new_Todo" RENAME TO "Todo";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
